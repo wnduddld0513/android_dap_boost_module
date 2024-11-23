4d9597b112632afbe03f06fe4b51d608eb92702d
Juyoung Hwang <wnduddld0513@gmail.com>
2024-11-23 19:21:21 +0900

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,21 +1,11 @@
 #!/system/bin/sh
 
-chown -R root:root /sys/devices/system/cpu
 chown -R root:root /sys/class/devfreq
 
 apply_settings() {
   # Apply schedtune boost settings
   echo 30 > /dev/stune/foreground/schedtune.boost
   echo 1 > /dev/stune/foreground/schedtune.sched_boost_no_override
-
-  # Set CPU Freq(CPU0~CPU3)
-  for cpu in 0 1 2 3; do
-    echo 300000 > /sys/devices/system/cpu/cpu$cpu/cpufreq/scaling_min_freq
-  done
-  # Set CPU Freq(CPU4~CPU7)
-  for cpu in 4 5 6 7; do
-    echo 300000 > /sys/devices/system/cpu/cpu$cpu/cpufreq/scaling_min_freq
-  done
 
   # Set GPU minFreq
   echo 465000000 > /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
